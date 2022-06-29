e114710a4eefe0c203fc26e479041357ac94d179
hanlopDev <hanafbomar@gmail.com>
2022-06-29 18:19:50 +0300

    first commit

---
+++ app/routers/twoforms.py
@@ -0,0 +1,12 @@
+from fastapi import Request, APIRouter
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="templates")
+
+router = APIRouter()
+
+@router.get("/twoforms", response_class=HTMLResponse)
+async def twoform_home(request: Request):
+    return templates.TemplateResponse("twoforms.html", {'request': request})
+
